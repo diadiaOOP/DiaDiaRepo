9ccd17ac99b296e56b7d584c76c59a9af2f9da77
AlekPiccio <alessio.piccione@hotmail.it>
2016-03-23 12:58:34 +0100

    added PartitaTest

--- test/it/uniroma3/diadia/PartitaTest.java
+++ test/it/uniroma3/diadia/PartitaTest.java
@@ -1,3 +1,10 @@
+/*Sono stati scritti i test che controllano Partita nel suo stato iniziale 
+ * in quanto i test non controllano il comportamento dei metodi 
+ * della classe Partita mentre DiaDia è in esecuzione, bensì servono per correggere
+ * eventuali errori commessi durante la correzione e scrittura di codice */
+
+
+
 package it.uniroma3.diadia;
 
 import static org.junit.Assert.*;
@@ -7,14 +14,36 @@ import org.junit.Test;
 
 public class PartitaTest {
 
+	private Partita partita;
+	
 	@Before
 	public void setUp() throws Exception {
-		
+		this.partita = new Partita();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetStanzaCorrente() {
+		assertEquals("Atrio", this.partita.getStanzaCorrente().getNome());
 	}
 
+	@Test
+	public void testGetGiocatore_NotNull() {
+		assertNotNull(this.partita.getGiocatore());
+	}
+	
+	/*@Test
+	public void testGetLabirinto() {
+		
+	}*/
+	
+	@Test
+	public void testVinta() {
+		assertFalse(this.partita.vinta());
+	}
+
+	@Test
+	public void testIsFinita() {
+		this.partita.setFinita();
+		assertTrue(this.partita.isFinita());
+	}
 }
