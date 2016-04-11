236fbb6e176f4e30ede1194220f433b69ced31af
badasteroid <amenichelli1991@gmail.com>
2016-04-11 10:39:57 +0200

    iniziati i test

---
+++ test/it/uniroma3/diadia/comandi/ComandoPosaTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package it.uniroma3.diadia.comandi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author andrea
+ *
+ */
+public class ComandoPosaTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link it.uniroma3.diadia.comandi.ComandoPosa#esegui(it.uniroma3.diadia.Partita)}.
+	 */
+	@Test
+	public void testEsegui() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link it.uniroma3.diadia.comandi.ComandoPosa#setParametro(java.lang.String)}.
+	 */
+	@Test
+	public void testSetParametro() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link it.uniroma3.diadia.comandi.ComandoPosa#getParametro()}.
+	 */
+	@Test
+	public void testGetParametro() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link it.uniroma3.diadia.comandi.ComandoPosa#getNome()}.
+	 */
+	@Test
+	public void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+}
